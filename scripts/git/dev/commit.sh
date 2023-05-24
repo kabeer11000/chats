bef7503e377b01c03416a29c006faae0eaf2aebc
kabeer11000 <kabeer@kabeersnetwork.tk>
2023-05-25 02:22:37 +0500

    27a1e4391b18 - Via: Auto Commit & Push Script | scripts/git/dev/commit.sh

--- scripts/git/dev/commit.sh
+++ scripts/git/dev/commit.sh
@@ -3,7 +3,7 @@ git add .
 a=$1
 if [ $# -eq 0 ]
   then
-    a=$(openssl rand -hex 12)
+    a=$(openssl rand -hex 6)
 fi
 #if [[ $b -eq 5 ]]; then a="$c"; else a="$d"; fi
 git commit -m "$a - Via: Auto Commit & Push Script | scripts/git/dev/commit.sh"
