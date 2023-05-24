807ffcdcb6eeb680401133fadffdd6acc2e589b9
kabeer11000 <kabeer@kabeersnetwork.tk>
2023-05-25 02:20:36 +0500

    11998b25fbaf89427c59bfa6

--- scripts/git/dev/commit.sh
+++ scripts/git/dev/commit.sh
@@ -1,9 +1,10 @@
 git add .
 
+a=$1
 if [ $# -eq 0 ]
   then
-    a= $(openssl rand -hex 12)
+    a=$(openssl rand -hex 12)
 fi
 #if [[ $b -eq 5 ]]; then a="$c"; else a="$d"; fi
-git commit -m $(echo a)
+git commit -m "$a"
 git push origin development
